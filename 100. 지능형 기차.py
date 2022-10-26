aa8b5e145fd10aede020ca07ac56d2432fa181da
jin5eok5 <ojs258@naver.com>
2022-10-26 23:19:21 +0900

    10일차 코딩연습

--- 100. 지능형 기차.py
+++ 100. 지능형 기차.py
@@ -3,7 +3,7 @@ import sys
 tot_peo = 0
 ans=[]
 for i in range(4):
-    in_peo, out_peo = map(int, sys.stdin.readline().split())
+    out_peo, in_peo = map(int, sys.stdin.readline().split())
     tot_peo -= out_peo
     tot_peo += in_peo
     ans.append(tot_peo)
